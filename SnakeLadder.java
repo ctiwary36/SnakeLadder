2243defce018f5edf3f58bca956b80907130bbf8
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-16 22:46:36 +0530

    Added:start position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,6 +3,10 @@ public class SnakeLadder{
 	public static void main(String[] args){
 	
 		System.out.println("Welcome to Snake and Ladder game");
+		
+		int PlayerPosition = 0;
+		
+		System.out.println("Game begins Player Position is: "+PlayerPosition);
 
 
 }
