de09fca81adcd87a7689938791ae900c19c0becb
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-16 23:00:48 +0530

    Added:Dice roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,10 +7,11 @@ public class SnakeLadder{
 		int PlayerPosition = 0;
 		
 		System.out.println("Game begins Player Position is: "+PlayerPosition);
-		
-	
+		DiceThrow();
+	}
+	public static void DiceThrow(){
+		int Dice = (int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("You got Dice Value "+Dice);
 
-
-}
-
+	}
 }
